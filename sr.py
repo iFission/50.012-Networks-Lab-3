cbf88919a58afaccf6abd2b124e370bc4cc79a3b
iFission <alexwwj@icloud.com>
2020-10-24 03:28:33 +0800

    feat: deduce bug in timer cancel

--- sr.py
+++ sr.py
@@ -17,16 +17,22 @@ class SelectiveRepeat:
         self.msg_handler = msg_handler
         self.sender_base = 0
         self.next_sequence_number = 0
-        self.set_timer()
-        self.window = [b''] * config.WINDOW_SIZE
-        self.expected_sequence_number = 0
+        self.timer_list = [
+            self.set_timer(i)
+            for i in range(self.next_sequence_number, config.WINDOW_SIZE)
+        ]
+        self.ack_list = [False] * config.WINDOW_SIZE
+        self.sender_buffer = [b''] * config.WINDOW_SIZE
         self.receiver_last_ack = b''
         self.is_receiver = True
+        self.receiver_base = 0
+        self.rcv_list = [False] * config.WINDOW_SIZE
+        self.rcv_buffer = [b''] * config.WINDOW_SIZE
         self.sender_lock = threading.Lock()
 
-    def set_timer(self):
-        self.timer = threading.Timer((config.TIMEOUT_MSEC / 1000.0),
-                                     self._timeout)
+    def set_timer(self, seq_num):
+        return threading.Timer((config.TIMEOUT_MSEC / 1000.0), self._timeout,
+                               {seq_num: seq_num})
 
     # "send" is called by application. Return true on success, false otherwise.
     def send(self, msg):
@@ -45,14 +51,22 @@ class SelectiveRepeat:
         packet = util.make_packet(msg, config.MSG_TYPE_DATA,
                                   self.next_sequence_number)
         packet_data = util.extract_data(packet)
-        self.window[self.next_sequence_number % config.WINDOW_SIZE] = packet
         util.log("Sending data: " + util.pkt_to_string(packet_data))
         self.network_layer.send(packet)
-        if self.sender_base == self.next_sequence_number:
-            if self.timer.is_alive(): self.timer.cancel()
-            self.set_timer()
-            self.timer.start()
-        self.next_sequence_number += 1
+        if self.next_sequence_number < self.sender_base + config.WINDOW_SIZE:
+            packet_offset_index = (self.next_sequence_number -
+                                   self.sender_base) % config.WINDOW_SIZE
+            print(packet_offset_index)
+            self.sender_buffer[packet_offset_index] = packet
+            packet_timer = self.timer_list[packet_offset_index]
+            if packet_timer.is_alive():
+                print('sender cancel timer after sending new packet')
+                packet_timer.cancel()
+            packet_timer = self.set_timer(self.next_sequence_number)
+            packet_timer.start()
+            self.next_sequence_number += 1
+        else:
+            pass
         self.sender_lock.release()
         return
 
@@ -62,63 +76,87 @@ class SelectiveRepeat:
         msg_data = util.extract_data(msg)
 
         if (msg_data.is_corrupt):
-            if (self.is_receiver):
-                if self.expected_sequence_number == 0:
-                    util.log("Packet received is corrupted. " +
-                             self.NO_PREV_ACK_MSG)
-                    return
-                self.network_layer.send(self.receiver_last_ack)
-                util.log("Received corrupted data. Resending ACK: " +
-                         util.pkt_to_string(
-                             util.extract_data(self.receiver_last_ack)))
             return
 
         # If ACK message, assume its for sender
         if msg_data.msg_type == config.MSG_TYPE_ACK:
             self.sender_lock.acquire()
-            self.sender_base = msg_data.seq_num + 1
-            if (self.sender_base == self.next_sequence_number):
-                util.log(
-                    "Received ACK with seq # matching the end of the window: "
-                    + util.pkt_to_string(msg_data) + ". Cancelling timer.")
-                self.timer.cancel()
-            else:
-                util.log(
-                    "Received ACK: " + util.pkt_to_string(msg_data) +
-                    ". There are messages in-flight. Restarting the timer.")
-                if self.timer.is_alive(): self.timer.cancel()
-                self.set_timer()
-                self.timer.start()
+            packet_offset_index = (msg_data.seq_num -
+                                   self.sender_base) % config.WINDOW_SIZE
+            self.ack_list[packet_offset_index] = True
+            print('sender cancel timer after receiving ack')
+
+            util.log("Received ACK with seq #" + util.pkt_to_string(msg_data) +
+                     ". Cancelling timer.")
+            self.timer_list[packet_offset_index].cancel()
+
+            for timer in self.timer_list:
+                print(timer.is_alive())
+
+            # check if the packet right after sendbase is ack'd
+            # if yes, move sendbase
+            # update the arrays (timer_list, ack_list) accordingly
+            # by removing first element, and add empty timer, and False ack
+            while self.ack_list[0] == True:
+                self.sender_base += 1
+                self.timer_list = self.timer_list[1:] + [
+                    self.set_timer(msg_data.seq_num)
+                ]
+                self.ack_list = self.ack_list[1:] + [False]
+                util.log(f"Updated send base to {self.sender_base}")
+
             self.sender_lock.release()
+
         # If DATA message, assume its for receiver
         else:
             assert msg_data.msg_type == config.MSG_TYPE_DATA
             util.log("Received DATA: " + util.pkt_to_string(msg_data))
-            if msg_data.seq_num == self.expected_sequence_number:
-                self.msg_handler(msg_data.payload)
-                ack_pkt = util.make_packet(b'', config.MSG_TYPE_ACK,
-                                           self.expected_sequence_number)
+
+            ack_pkt = util.make_packet(b'', config.MSG_TYPE_ACK,
+                                       msg_data.seq_num)
+
+            if msg_data.seq_num in range(
+                    self.receiver_base,
+                    self.receiver_base + config.WINDOW_SIZE):
                 self.network_layer.send(ack_pkt)
-                self.receiver_last_ack = ack_pkt
-                self.expected_sequence_number += 1
                 util.log("Sent ACK: " +
                          util.pkt_to_string(util.extract_data(ack_pkt)))
+                packet_offset_index = (msg_data.seq_num -
+                                       self.receiver_base) % config.WINDOW_SIZE
+                self.rcv_list[packet_offset_index] = True
+
+                if msg_data.seq_num != self.receiver_base:
+                    self.rcv_buffer[packet_offset_index] = msg.data
+
+                else:
+                    while self.rcv_list[0] == True:
+                        self.msg_handler(self.rcv_buffer[0])
+                        self.receiver_base += 1
+                        self.rcv_list = self.rcv_list[1:] + [False]
+                        self.rcv_buffer = self.rcv_buffer[1:] + [b'']
+                        util.log(
+                            f"Updated receiver base to {self.receiver_base}")
+
+            elif msg_data.seq_num in range(
+                    self.receiver_base - config.WINDOW_SIZE - 1,
+                    self.receiver_base):
+
+                self.network_layer.send(ack_pkt)
+                util.log("Packet outside receiver window")
+                util.log("Sent ACK: " +
+                         util.pkt_to_string(util.extract_data(ack_pkt)))
+
             else:
-                if self.expected_sequence_number == 0:
-                    util.log("Packet received is out of order. " +
-                             self.NO_PREV_ACK_MSG)
-                    return
-                util.log("DATA message had unexpected sequence #" +
-                         str(int(msg_data.seq_num)) +
-                         ". Resending ACK message with sequence # " +
-                         str(int(self.expected_sequence_number - 1)) + ".")
-                self.network_layer.send(self.receiver_last_ack)
+                return
+
         return
 
     # Cleanup resources.
     def shutdown(self):
         if not self.is_receiver: self._wait_for_last_ACK()
-        if self.timer.is_alive(): self.timer.cancel()
+        for timer in self.timer_list:
+            print(timer.is_alive())
+            if timer.is_alive(): timer.cancel()
         util.log("Connection shutting down...")
         self.network_layer.shutdown()
 
@@ -128,19 +166,17 @@ class SelectiveRepeat:
                      str(int(self.next_sequence_number - 1)) + ".")
             time.sleep(1)
 
-    def _timeout(self):
-        util.log(
-            "Timeout! Resending all packets in window. Resending packets with seq #s "
-            + str(self.sender_base) + "-" +
-            str(self.next_sequence_number - 1) + ".")
+    def _timeout(self, seq_num):
+        util.log(f"Timeout! Resending packet {seq_num}")
         self.sender_lock.acquire()
-        if self.timer.is_alive(): self.timer.cancel()
-        self.set_timer()
-        for i in range(self.sender_base, self.next_sequence_number):
-            pkt = self.window[(i % config.WINDOW_SIZE)]
-            self.network_layer.send(pkt)
-            util.log("Resending packet: " +
-                     util.pkt_to_string(util.extract_data(pkt)))
-        self.timer.start()
+        packet_offset_index = (seq_num - self.sender_base) % config.WINDOW_SIZE
+        packet_timer = self.timer_list[packet_offset_index]
+        if packet_timer.is_alive(): packet_timer.cancel()
+        packet_timer = self.set_timer(seq_num)
+        pkt = self.sender_buffer[packet_offset_index]
+        self.network_layer.send(pkt)
+        util.log("Resending packet: " +
+                 util.pkt_to_string(util.extract_data(pkt)))
+        packet_timer.start()
         self.sender_lock.release()
         return
