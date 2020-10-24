d7ecd2ae45c191801d8974f0512d98873eec3fe6
iFission <alexwwj@icloud.com>
2020-10-25 02:42:47 +0800

    feat: fix packet_offset_index

--- file_test.py
+++ file_test.py
@@ -1,6 +1,7 @@
 import os
 import time
 import pexpect
+import sys
 
 
 def test_sr():
@@ -11,8 +12,34 @@ def test_sr():
     sender = pexpect.spawn(f"python3 file_sender.py sr {file_send}")
 
     # wait for 1 sec, then terminate receiver
-    time.sleep(1)
+    time.sleep(2)
     receiver.sendcontrol('c')
+    time.sleep(.1)
+    receiver.close()
+    sender.close()
+
+    # compare output
+    output = os.popen(f"md5 {file_send} {file_receive}").read()
+
+    file_send_checksum = output.split("\n")[0].split()[-1]
+    file_receive_checksum = output.split("\n")[1].split()[-1]
+
+    assert file_send_checksum == file_receive_checksum
+
+
+def test_sr_picture():
+    file_send = "ss.png"
+    file_receive = "ss_rcv.png"
+
+    receiver = pexpect.spawn(f"python3 file_receiver.py sr {file_receive}")
+    sender = pexpect.spawn(f"python3 file_sender.py sr {file_send}")
+
+    # wait for 1 sec, then terminate receiver
+    time.sleep(6)
+    # receiver.expect("Received MSG of length119")
+    # sender.expect("Time used")
+    receiver.sendcontrol('c')
+    time.sleep(2)
     receiver.close()
     sender.close()
 
